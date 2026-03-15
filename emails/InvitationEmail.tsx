2cc25aa60ceb4607e85bed98c8a464cc3dc80f4f
Aditya Shelke <adi.cs.night@gmail.com>
2026-03-15 23:19:43 +0530

    Standardize button loading states
    Refactored Button component to include a standard spinner loading prop and updated all major modals, forms, and pages. Also fixed minor linting issues in QuickLog and InvitationEmail.

--- emails/InvitationEmail.tsx
+++ emails/InvitationEmail.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-page-custom-font */
 import {
   Body,
   Button,
@@ -11,6 +10,7 @@ import {
   Preview,
   Section,
   Text,
+  Font,
 } from '@react-email/components';
 import * as React from 'react';
 
@@ -39,9 +39,15 @@ export function InvitationEmail({
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap"
+        <Font
+          fontFamily="Space Grotesk"
+          fallbackFontFamily="Helvetica"
+          webFont={{
+            url: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap',
+            format: 'woff2',
+          }}
+          fontWeight={400}
+          fontStyle="normal"
         />
       </Head>
       <Preview>
